54722e32404f8dd442454b72bd4b4a355b728fca
MaxRem <maxzheng0302@gmail.com>
2025-06-14 15:24:01 +0800

    Final version 2

--- src/VGAInterface/vga2monitor.sv
+++ src/VGAInterface/vga2monitor.sv
@@ -29,29 +29,18 @@ module VGA2monitor #(
 
     localparam VGA_1600x900_H_A = 24, VGA_1600x900_H_B = 80, VGA_1600x900_H_D = 96;
     localparam VGA_1600x900_V_A = 1, VGA_1600x900_V_B = 3, VGA_1600x900_V_D = 96;
-    localparam H_A = (H_SIZE == 640 && V_SIZE == 480 && FRAME_RATE == 60) ? VGA_640x480_H_A :
-                     (H_SIZE == 1600 && V_SIZE == 900 && FRAME_RATE == 60) ? VGA_1600x900_H_A :
-                     VGA_1600x900_H_A; // Default value
+    
+    localparam H_A = VGA_1600x900_H_A; // Default value
 
-    localparam H_B = (H_SIZE == 640 && V_SIZE == 480 && FRAME_RATE == 60) ? VGA_640x480_H_B :
-                     (H_SIZE == 1600 && V_SIZE == 900 && FRAME_RATE == 60) ? VGA_1600x900_H_B :
-                     VGA_1600x900_H_B; // Default value
+    localparam H_B = VGA_1600x900_H_B; // Default value
 
-    localparam H_D = (H_SIZE == 640 && V_SIZE == 480 && FRAME_RATE == 60) ? VGA_640x480_H_D :
-                     (H_SIZE == 1600 && V_SIZE == 900 && FRAME_RATE == 60) ? VGA_1600x900_H_D :
-                     VGA_1600x900_H_D; // Default value
+    localparam H_D = VGA_1600x900_H_D; // Default value
 
-    localparam V_A = (H_SIZE == 640 && V_SIZE == 480 && FRAME_RATE == 60) ? VGA_640x480_V_A :
-                     (H_SIZE == 1600 && V_SIZE == 900 && FRAME_RATE == 60) ? VGA_1600x900_V_A :
-                     VGA_1600x900_V_A; // Default value
+    localparam V_A = VGA_1600x900_V_A; // Default value
 
-    localparam V_B = (H_SIZE == 640 && V_SIZE == 480 && FRAME_RATE == 60) ? VGA_640x480_V_B :
-                     (H_SIZE == 1600 && V_SIZE == 900 && FRAME_RATE == 60) ? VGA_1600x900_V_B :
-                     VGA_1600x900_V_B; // Default value
+    localparam V_B = VGA_1600x900_V_B; // Default value
 
-    localparam V_D = (H_SIZE == 640 && V_SIZE == 480 && FRAME_RATE == 60) ? VGA_640x480_V_D :
-                     (H_SIZE == 1600 && V_SIZE == 900 && FRAME_RATE == 60) ? VGA_1600x900_V_D :
-                     VGA_1600x900_V_D; // Default value
+    localparam V_D = VGA_1600x900_V_D; // Default value
 
     // FSM for sync/control signals generation
     parameter S_B = 0; // back porch
